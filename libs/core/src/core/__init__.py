c4b9df4bd35aec92802befe39fec7073d02707e0
Marco Wegner <673439+marvey11@users.noreply.github.com>
2026-08-24 12:14:33 +0000

    refactor: introduce stock service

--- libs/core/src/core/__init__.py
+++ libs/core/src/core/__init__.py
@@ -1,9 +1,11 @@
 from .models import StockMetadata
 from .repository import JsonStockRepository, RepositoryCorruptedError, StockRepository
+from .services import StockService
 
 __all__ = [
     "JsonStockRepository",
     "RepositoryCorruptedError",
     "StockMetadata",
     "StockRepository",
+    "StockService",
 ]
